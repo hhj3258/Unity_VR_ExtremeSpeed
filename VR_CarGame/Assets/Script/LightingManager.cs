2d2624515dc87d663ea049bc6689aa82fbc5a573
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-05-13 20:26:49 +0900

    프로토타입 업로드
    GVREmulator 사용

---
+++ VR_CarGame/Assets/Script/LightingManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingManager : MonoBehaviour
+{
+    public List<Light> lights;
+    
+    public virtual void ToggleHeadLights()
+    {
+        
+        foreach (Light light in lights)
+        {
+            if(light.intensity == 0)
+            {
+                light.intensity = 2;
+            }
+            else
+            {
+                light.intensity = 0;
+            }
+            //temp = light.intensity == 0 ? 2 : 0;
+            //light.intensity = temp;
+        }
+    }
+
+
+}
